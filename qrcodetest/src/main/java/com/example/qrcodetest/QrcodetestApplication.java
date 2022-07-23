d0955fa674233aa807825c6b912fac9339c2b730
rawnl <rawnaq.naili@univ-constantine2.dz>
2022-07-23 13:13:55 +0100

    first commit

---
+++ qrcodetest/src/main/java/com/example/qrcodetest/QrcodetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrcodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodetestApplication.class, args);
+	}
+
+}
---
+++ qrcodetest/src/test/java/com/example/qrcodetest/QrcodetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrcodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
